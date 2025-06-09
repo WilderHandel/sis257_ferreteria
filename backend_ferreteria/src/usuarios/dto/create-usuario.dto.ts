1b3589507338c3e898df1f9ba0b02ce672e1c76a
nayely limachi <nayelylimachi002@gmail.com>
2025-06-09 16:18:18 -0400

    crud de la tabla usuarios

--- backend_ferreteria/src/usuarios/dto/create-usuario.dto.ts
+++ backend_ferreteria/src/usuarios/dto/create-usuario.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUsuarioDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
+
+export class CreateUsuarioDto {
+  @ApiProperty()
+  @IsNotEmpty({ message: 'El campo usuario es obligatorio' })
+  @IsString({ message: 'El campo usuario debe ser de tipo cadena' })
+  @MaxLength(15, { message: 'El campo usuario no debe ser mayor a 15 caracteres' })
+  readonly usuario: string;
+}
--- backend_ferreteria/src/usuarios/entities/usuario.entity.ts
+++ backend_ferreteria/src/usuarios/entities/usuario.entity.ts
@@ -1,10 +1,25 @@
 import { Venta } from 'src/ventas/entities/venta.entity';
-import { Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity('usuarios')
 export class Usuario {
   @PrimaryGeneratedColumn('identity')
   id: number;
+
+  @Column('varchar', { length: 15 })
+  usuario: string;
+
+  @Column('varchar', { length: 250 })
+  contraseña: string;
+
+  @CreateDateColumn({ name: 'fecha_creacion' })
+  fechaCreacion: Date;
+  
+  @UpdateDateColumn({ name: 'fecha_modificacion' })
+  fechaModificacion: Date;
+  
+  @DeleteDateColumn({ name: 'fecha_eliminacion' })
+  fechaEliminacion: Date;
 
   @OneToMany(() => Venta, (venta) => venta.usuario)
   ventas: Venta[];
