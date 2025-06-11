498ccf6262ec2c2c37815f23d0aedf9da4f5af83
unknown <gloriavivianavidalcolque@gmail.com>
2025-06-11 11:50:58 -0400

    Fix: Arreglo de la introducción del Template

--- frontend_ferreteria/src/App.vue
+++ frontend_ferreteria/src/App.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router'
 import '@/assets/js/jquery.min.js'
-import '@/assets/js/popper.min.js'
-import '@/assets/js/bootstrap.bundle.min.js'
-import '@/assets/js/jquery-3.0.0.min.js'
-import '@/assets/js/plugin.js'
-import '@/assets/js/jquery.mCustomScrollbar.concat.min.js'
 import '@/assets/js/custom.js'
 import MainHeader from './components/MainHeader.vue'
 import MainFooter from './components/MainFooter.vue'
-import { RouterView } from 'vue-router'
 </script>
 
 <template>
@@ -20,8 +15,10 @@ import { RouterView } from 'vue-router'
 </template>
 
 <style>
-@import '@assets/css/bootstrap.min.css';
-@import '@assets/css/style.css';
-@import '@assets/css/responsive.css';
-@import '@assets/css/jquery.mCustomScrollbar.min.css';
+@import '@/assets/css/jquery-ui.css';
+@import '@/assets/css/bootstrap.min.css';
+@import '@/assets/css/style.css';
+@import '@/assets/css/responsive.css';
+@import '@/assets/css/jquery.mCustomScrollbar.min.css';
+@import '@/assets/css/swiper.min.css';
 </style>
--- frontend_ferreteria/src/components/MainHeader.vue
+++ frontend_ferreteria/src/components/MainHeader.vue
@@ -1,42 +1,39 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import { useRoute } from 'vue-router'
+const route = useRoute()
 </script>
 
 <template>
-
-  <head>
-    <!-- basic -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- mobile metas -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-    <!-- site metas -->
-    <title>Ferrreteria Bicentenario</title>
-    <meta name="description"
-      content="Ferretería Bicentenario - Herramientas y materiales de calidad para tus proyectos de construcción.">
-    <meta name="keywords" content="ferretería, herramientas, construcción, tornillos, materiales, bricolaje, Sucre">
-    <meta name="author" content="Ferrreteria Bicentenario">
-  </head>
-  <header>
-    <!-- header inner -->
-    <div class="header">
-      <div class="head_top">
-        <div class="container">
-          <div class="row">
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-              <div class="top-box">
-                <ul class="sociel_link">
-                  <RouterLink to="/" class="fa fa-facebook-f"></RouterLink>
-                  <RouterLink to="/" class="fa fa-twitter"></RouterLink>
-                  <RouterLink to="/" class="fa fa-instagram"></RouterLink>
-                  <RouterLink to="/" class="fa fa-linkedin"></RouterLink>
-                </ul>
+  <div>
+    <header>
+      <!-- header inner -->
+      <div class="header">
+        <div class="head_top">
+          <div class="container">
+            <div class="row">
+              <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                <div class="top-box">
+                  <ul class="sociel_link">
+                    <li>
+                      <a href="#"><i class="fa fa-facebook-f"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-twitter"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-instagram"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-linkedin"></i></a>
+                    </li>
+                  </ul>
+                </div>
               </div>
-            </div>
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-              <div class="top-box">
-                <p>Soluciones fuertes para grandes proyectos. </p>
+              <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                <div class="top-box">
+                  <p>Soluciones fuertes para grandes proyectos.</p>
+                </div>
               </div>
             </div>
           </div>
@@ -47,7 +44,9 @@ import { RouterLink } from 'vue-router'
           <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
             <div class="full">
               <div class="center-desk">
-                <div class="logo"> <a href="index.html"><img src="src/assets/images/logo.jpg" alt="logo" /></a> </div>
+                <div class="logo">
+                  <RouterLink to="/"><img src="@/assets/images/logo.jpg" alt="logo" /></RouterLink>
+                </div>
               </div>
             </div>
           </div>
@@ -56,28 +55,88 @@ import { RouterLink } from 'vue-router'
               <div class="limit-box">
                 <nav class="main-menu">
                   <ul class="menu-area-main">
-                    <li class="active">
-                    </li>
-                    <RouterLink to="/">Home</RouterLink>
-                    <RouterLink to="/about">About</RouterLink>
-                    <RouterLink to="/producto">Productos</RouterLink>
-                    <RouterLink to="/clientes">Clientes</RouterLink>
-                    <RouterLink to="/ventasdetalles">Venta Detalle</RouterLink>
-                    <RouterLink to="/proveedores">Proveedores</RouterLink>
-                    <RouterLink to="/categorias">Categorías</RouterLink>
+                    <li class="active"><RouterLink to="/">Inicio</RouterLink></li>
+                    <li><RouterLink to="/about">Acerca de</RouterLink></li>
+                    <li><RouterLink to="/producto">Productos</RouterLink></li>
+                    <li><RouterLink to="/proveedores">Proveedores</RouterLink></li>
+                    <li><RouterLink to="/categorias">Categorias</RouterLink></li>
                   </ul>
                 </nav>
               </div>
             </div>
           </div>
-          <!--<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2">
-            <li><a class="buy" href="#">Iniciar Sesión</a></li>
-          </div>-->
+          <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2">
+            <RouterLink to="/login" class="buy">Iniciar Sesión</RouterLink>
+          </div>
         </div>
       </div>
-    </div>
-    <!-- end header inner -->
-  </header>
+      <!-- end header inner -->
+    </header>
+    <!-- end header -->
+    <section class="slider_section" v-if="route.path === '/'">
+      <div id="main_slider" class="carousel slide banner-main" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <RouterLink to="/"><img src="@/assets/images/banner2.jpg" alt="banner" /></RouterLink>
+            <div class="container">
+              <div class="carousel-caption relative">
+                <h1>
+                  Nuestro <br />
+                  <strong class="black_bold">Último </strong><br />
+                  <strong class="yellow_bold">Producto </strong>
+                </h1>
+                <p>
+                  Herramientas de calidad<br />
+                  todos los productos al alcance del consumidor
+                </p>
+                <a href="#">Ver más Productos</a>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <RouterLink to="/"><img class="second-slide" src="@/assets/images/banner2.jpg" alt="Second slide" /></RouterLink>
+            <div class="container">
+              <div class="carousel-caption relative">
+                <h1>
+                  Nuestro <br />
+                  <strong class="black_bold">Último </strong><br />
+                  <strong class="yellow_bold">Producto </strong>
+                </h1>
+                <p>
+                  Herramientas de calidad<br />
+                  todo los productos al alcance del consumidor
+                </p>
+                <a href="#">Ver más Productos</a>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+           <RouterLink to="/"><img src="@/assets/images/banner2.jpg" alt="banner" /></RouterLink>
+            <div class="container">
+              <div class="carousel-caption relative">
+                <h1>
+                  Nuestro <br />
+                  <strong class="black_bold">Último </strong><br />
+                  <strong class="yellow_bold">Producto </strong>
+                </h1>
+                <p>
+                  Herramientas de calidad<br />
+                  todo los productos al alcance del consumidor
+                </p>
+                <a href="#">Ver más Productos</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
+          <i class="fa fa-angle-right"></i>
+        </a>
+        <a class="carousel-control-next" href="#main_slider" role="button" data-slide="next">
+          <i class="fa fa-angle-left"></i>
+        </a>
+      </div>
+    </section>
+  </div>
 </template>
 
 <style scoped></style>
