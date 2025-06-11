97b9d586a5f7dcf34290a4a4517bb935a9d1fec1
Wil <wilderhandel@gmail.com>
2025-06-11 12:43:53 -0400

    Fix: Corrección para ingresar empleados e inicio en frontend del logi

--- frontend_ferreteria/src/plugins/axios.ts
+++ frontend_ferreteria/src/plugins/axios.ts
@@ -2,7 +2,10 @@ import Axios from 'axios'
 
 const axios = Axios.create({
   baseURL: import.meta.env.VITE_BASE_URL_ENDPOINT,
-  headers: { 'Content-type': 'application/json' },
+  headers: {
+    'Content-type': 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsImlhdCI6MTc0OTY2MDA0MiwiZXhwIjoxNzQ5NjYwMTAyfQ.m8t8wWZ6ulS7kbNWFvU2InLFQmduTurMlam8AdukaDc',
+  },
 })
 
 export default axios
